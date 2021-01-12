de93e5b13377d52fcb8819767f79d5b7a3b75345
msettimi13 <55293931+msettimi13@users.noreply.github.com>
2021-01-12 14:30:00 -0500

    changes

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const http = require('http'); // like a PHP require
 // require is more or less the same as a JS import
 
 const hostname = '127.0.0.1'; //this is local host
-const port = 3000; //localhost:3000
+const port = process.env.PORT || 3000; //localhost:3000
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((req, res) => { //req means request res is response
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hi World');
